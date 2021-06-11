bb962d54f5b215b3f7dd2e368dc032b223c3b11d
oskar1410 <Timoteusz1>
2021-06-11 16:46:29 +0200

    decomp generator

---
+++ Randomizer/DecompositionGenerator.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using CommonCode;
+
+namespace Randomizer
+{
+    public class DecompositionGenerator
+    {
+        private int tw, bagsCount, nodesCount;
+        private Graph graph;
+        private DecompositionNode root = new DecompositionNode(new List<int>());
+
+        public DecompositionGenerator(int tw, int bagsCount, int nodesCount)
+        {
+            this.tw = tw;
+            this.bagsCount = bagsCount;
+            this.nodesCount = nodesCount;
+        }
+
+        public void Generate()
+        {
+            Random random = new Random();
+            int numberOfBags = bagsCount - 1;
+            int maxKidsCount = bagsCount - 1;
+            Queue<DecompositionNode> queue = new Queue<DecompositionNode>();
+            List<DecompositionNode> bags = new List<DecompositionNode>()
+            queue.Enqueue(this.root);
+            bags.Add(this.root);
+            // tworzenie struktury pustych worków z połączeniami
+            while (numberOfBags > 0)
+            {
+                var bag = queue.Dequeue();
+                int kidsCount = random.Next(1, maxKidsCount);
+                if (numberOfBags - kidsCount < 0)
+                    kidsCount = numberOfBags;
+                numberOfBags -= kidsCount;
+                for (int i = 0; i < kidsCount; ++i)
+                {
+                    var kid = new DecompositionNode(new List<int>());
+                    kid.Parent = bag;
+                    bag.Children.Add(kid);
+                    bags.Add(kid);
+                    queue.Enqueue(kid);
+                }
+            }
+            for (int i = 0; i < nodesCount; ++i)
+            {
+                //wylosuj bag dopóki nie znajdziesz wolnego
+                int bagNumber;
+                while (true)
+                {
+                    bagNumber = random.Next(0, numberOfBags);
+                    if (bags[bagNumber].Vertices.Count < tw - 1)
+                        break;
+                }
+                bags[bagNumber].Vertices.Add(i);
+            }
+            // tworzenie grafu odpowiadającego dekompozycji
+            this.graph = new Graph(nodesCount);
+            foreach (var bag in bags) 
+                for (int i = 0; i < bag.Vertices.Count; ++i)
+                    for (int j = i + 1; j < bag.Vertices.Count; ++j)
+                        graph.AddEdge(i, j);
+        }
+    }
+}
